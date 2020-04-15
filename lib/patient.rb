c53df6ae568a21796c10b8341de459c0d27af4cd
Abdul Khan <akhan17@outlook.com>
2020-04-15 00:58:04 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,19 @@
 class Patient
   
-  attr_accessor :name 
+  attr_accessor :name, :date, :doctor
+  
+  @@all = []
   
   def initialize(name)
     @name = name
   end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(date, doctor)
+    Appointment.new('Friday, January 32nd',self,doctor)
+  end
   
 end 
